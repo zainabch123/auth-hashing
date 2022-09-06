78d55408657444a1f5693176c7c4ad0a7691e2d1
Nathan King <nathanking3590@gmail.com>
2022-09-06 08:40:14 +0100

    Update registrations.js

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -6,16 +6,14 @@ const jwt = require('jsonwebtoken');
 const prisma = require('../utils/prisma.js')
 
 router.post('/', async (req, res) => {
-    const { username, password } = req.body;
-
-    const createdUser = await prisma.user.create({
-        data: {
-            username,
-            password
-        }
-    });
-
-    res.json({ data: createdUser });
+    // Get the username and password from request body
+    
+    // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
+    
+    // Save the user using the prisma user model, setting their password to the hashed version
+    
+    // Respond back to the client with the created users username and id
+    res.status(201).json({ user: undefined })
 });
 
 module.exports = router;
