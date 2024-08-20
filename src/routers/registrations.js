a373817c0fa77a3188ca64986460ccfd58bbe016
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-08-20 16:09:49 +0100

    Completed user registration function

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -7,13 +7,21 @@ const prisma = require('../utils/prisma.js')
 
 router.post('/', async (req, res) => {
     // Get the username and password from request body
+    const { username, password } = req.body;
     
     // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
+    const hashedPassword = await bcrypt.hash(password, 10);
     
     // Save the user using the prisma user model, setting their password to the hashed version
+     const user = await prisma.user.create({
+       data: {
+         username: username,
+         password: hashedPassword, // store the hashed password
+       },
+     });
     
     // Respond back to the client with the created users username and id
-    res.status(201).json({ user: undefined })
+    res.status(201).json({ user: user })
 });
 
 module.exports = router;
