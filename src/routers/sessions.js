aab3df43d06ff70640ada61cdc81338ade25bbf7
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-08-20 16:19:15 +0100

    Completed user login function

--- src/routers/sessions.js
+++ src/routers/sessions.js
@@ -1,22 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = require('../utils/prisma.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const prisma = require("../utils/prisma.js");
+const secret = process.env.JWT_SECRET;
 
-router.post('/', async (req, res) => {
-    const { username, password } = req.body;
-    // Get the username and password from the request body
+router.post("/", async (req, res) => {
+  // Get the username and password from the request body
+  const { username, password } = req.body;
 
-    // Check that a user with that username exists in the database
-    // Use bcrypt to check that the provided password matches the hashed password on the user
-    // If either of these checks fail, respond with a 401 "Invalid username or password" error
+  // Check that a user with that username exists in the database
+  const user = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+    select: {
+      id: true,
+      password: true,
+    },
+  });
 
-    // If the user exists and the passwords match, create a JWT containing the username in the payload
-    // Use the JWT_SECRET environment variable for the secret key
+  if (!user) {
+    return res.status(401).json({ error: "Invalid username or password" });
+  }
 
-    // Send a JSON object with a "token" key back to the client, the value is the JWT created
+  // Use bcrypt to check that the provided password matches the hashed password on the user
+  // If either of these checks fail, respond with a 401 "Invalid username or password" error
+  const validPassword = await bcrypt.compare(password, user.password);
+
+  if (!user || !validPassword) {
+    return res.status(401).json({ error: "Invalid username or password" });
+  }
+
+  // If the user exists and the passwords match, create a JWT containing the username in the payload
+  // Use the JWT_SECRET environment variable for the secret key
+  const token = jwt.sign({ username: user.username }, secret);
+
+  // Send a JSON object with a "token" key back to the client, the value is the JWT created
+  res.json({ token: token });
 });
 
 module.exports = router;
